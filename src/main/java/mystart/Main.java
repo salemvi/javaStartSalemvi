f6b57a7f6cc68353b665d6ae0a7c3ecc8880f569
SalemVZ <zamanovvr@gmail.com>
2023-03-05 19:23:25 +0300

    start test rep+

--- src/main/java/mystart/Main.java
+++ src/main/java/mystart/Main.java
@@ -1,8 +1,8 @@
 package mystart;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println(12345);
+    public static void main(String[] args) { //старт с нуля
+
         System.out.println("Hello world!");
     }
 }
